d4998eb030a9d81af1975d4c5bd3c60f01212f7a
hewhodevs <hewhodevs@outlook.com>
2021-08-17 16:35:36 +0930

    updated comments

--- dates.js
+++ dates.js
@@ -1,19 +1,16 @@
 // ------------------------------------------
 // func formatYYYYMMDD
 // ------------------------------------------
-// Takes a date object, and returns a string formatted as YYYY/MM/DD
+// Takes a date object, and returns a string formatted as YYYY-MM-DD
 // @date - a date object that will not be mutated by this function.
 function formatYYYYMMDD(date) {
   let utcDate = date.getUTCDate();
-  let month = date.getMonth() + 1; // as getMonth starts from 0 for Jan
+  // as getMonth starts from 0 for Jan
+  let month = date.getMonth() + 1;
   const year = date.getFullYear();
   // Add a leading 0 to single digit days / months
-  if (utcDate < 10) {
-    utcDate = `0${utcDate}`;
-  }
-  if (month < 10) {
-    month = `0${month}`
-  }
+  if (utcDate < 10) { utcDate = `0${utcDate}` }
+  if (month < 10) { month = `0${month}` }
   return `${year}-${month}-${utcDate}`;
 }
 
